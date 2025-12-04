bc6510911a278cf9ed47ebe598f48d16e8563ecf
Kyanni <kyanni.gonzalez2027@gbwl.org>
2025-12-04 19:22:15 +0000

    got up to Demogrogan Drive

--- Main.java
+++ Main.java
@@ -16,11 +16,13 @@ import java.util.Scanner;
       if (command.equals("s") ) {
          //NEXT BRANCH...
          System.out.println("Walk down Demogrogan Drive.");
+         System.out.println("WOAH! you have tripped into a portal and landed in...CALIFORNIA WHAT");
       }
 
       else if (command.equals("e") ){
          System.out.println("You've got a bike, and a backpack full of esstentials! Now bike through the forest.");  
          System.out.println("you discovered a creepy, slimy, red, hole, Do you want to go through and explore");
+         command = getCommand("explore or stay curious"); 
           }
         
         if (command.equals("y") ){
@@ -39,10 +41,19 @@ import java.util.Scanner;
          command = getCommand("Left or Right? (l or r"); 
 
          if (command.equals("l") ) {
-            System.out.println("You need to stall in order for the plan to succssed.. ");
+            System.out.println("You need to stall in order for the plan to succssed(s or d"); 
+            command = getCommand("s or d"); 
+         
+          if(command.equals ("s")){
+            System.out.println("You outsmarted the Mindflayer...(for now)! Nice job on dropping a speaker while Steve was driving, it ran off to the sound"); 
+            System.out.println("Head to the Starcourt Mall, and meet up with eveyone");
+         } 
+            else if (command.equals("d")){
+               System.out.println("The Mindflayer ripped open the roof and killed you all, mission failed");
             
-
-           
+             }
+         }
+         
          }
          else if (command.equals("r")) {
                System.out.println("The demogrogans are chasing the car, you need to get away.");
@@ -59,7 +70,7 @@ import java.util.Scanner;
 
 
 
-    } // END OF MAIN METHOD
+     // END OF MAIN METHOD
     
     /** Method to prompt & process input for the next command
       * @param prompt to display to user, asks for specific input
